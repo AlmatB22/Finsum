745953a31ce7d3a41d6dabf02fe0af05e8b2e306
almatbolatbekov <almat.bolatbek22@gmail.com>
2025-11-15 18:02:43 -0800

    Added Home and Favourites pages

---
+++ frontend/app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import { HeaderTitle } from "@react-navigation/elements";
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+    return (
+        <Tabs screenOptions={{ headerShown: false}}>
+            <Tabs.Screen name='index'/>
+            <Tabs.Screen name='favourites' options={{
+                
+            }}/>
+        </Tabs>
+    )
+}
